27481eb81cdf6378ca602e11eb3e832fecb3be0a
Dominik Spicher <dominikspicher@gmail.com>
2023-08-07 00:14:36 +0200

    parser/index: remove duplicated or-operand

--- src/parser/index.rs
+++ src/parser/index.rs
@@ -134,7 +134,7 @@ pub fn get_block_index(
         db_iter.current(&mut key, &mut value);
         if is_block_index_record(&key) {
             let record = BlockIndexRecord::from(&key[1..], &value)?;
-            if record.status & (BLOCK_VALID_CHAIN | BLOCK_HAVE_DATA | BLOCK_VALID_CHAIN) > 0 {
+            if record.status & (BLOCK_VALID_CHAIN | BLOCK_HAVE_DATA) > 0 {
                 block_index.insert(record.height, record);
             }
         }
