d2658c70ec137488175839ab6d710b5752db30ea
Dominik Spicher <dominikspicher@gmail.com>
2023-08-14 17:17:00 +0200

    remove callbacks
    We want to migrate this crate into a direction
    where we provide out-of-the-box all of the
    ways in which a header and / or block could
    be analyzed. The user can then switch these
    analyses on or off. For this functionality,
    we don't need the overkill flexibility of
    trait objects.
    
    The goal is to provide a much more authoritative
    block analysis tool, closer to an enhanced block
    explorer than the ad-hoc analyses implemented before.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,5 @@
 use std::time::{Duration, Instant};
 
-use crate::callbacks::Callback;
 use crate::parser::chain::ChainStorage;
 use crate::ParserOptions;
 
@@ -32,34 +31,32 @@ impl WorkerStats {
 pub struct BlockchainParser {
     chain_storage: ChainStorage, // Hash storage with the longest chain
     stats: WorkerStats,          // struct for thread management & statistics
-    callback: Box<dyn Callback>,
     cur_height: u64,
 }
 
 impl BlockchainParser {
     /// Instantiates a new Parser.
     #[must_use]
-    pub fn new(options: ParserOptions, chain_storage: ChainStorage) -> Self {
+    pub fn new(options: &ParserOptions, chain_storage: ChainStorage) -> Self {
         tracing::info!(target: "parser", "Parsing {} blockchain ...", options.coin.name);
         Self {
             chain_storage,
             stats: WorkerStats::new(options.range.start),
-            callback: options.callback,
             cur_height: options.range.start,
         }
     }
 
-    pub fn start(&mut self) -> anyhow::Result<()> {
+    pub fn start(&mut self) {
         tracing::debug!(target: "parser", "Starting worker ...");
 
-        self.on_start(self.cur_height)?;
+        self.on_start(self.cur_height);
         while let Some(header) = self.chain_storage.get_header(self.cur_height) {
-            self.on_header(&header, self.cur_height)?;
+            Self::on_header(&header, self.cur_height);
             let block = self.chain_storage.get_block(self.cur_height).unwrap();
-            self.on_block(&block, self.cur_height)?;
+            self.on_block(&block, self.cur_height);
             self.cur_height += 1;
         }
-        self.on_complete(self.cur_height.saturating_sub(1))
+        self.on_complete(self.cur_height.saturating_sub(1));
     }
 
     /// Returns number of remaining blocks
@@ -70,44 +67,28 @@ impl BlockchainParser {
             .saturating_sub(self.cur_height)
     }
 
-    /// Triggers the on_start() callback and initializes state.
-    fn on_start(&mut self, height: u64) -> anyhow::Result<()> {
+    fn on_start(&mut self, height: u64) {
         let now = Instant::now();
         self.stats.started_at = now;
         self.stats.last_log = now;
         tracing::info!(target: "parser", "Processing blocks starting from height {} ...", height);
-        self.callback.on_start(height)?;
         tracing::trace!(target: "parser", "on_start() called");
-        Ok(())
     }
 
-    /// Triggers the on_block() callback and updates statistics.
-    fn on_header(
-        &mut self,
-        header: &bitcoin::blockdata::block::Header,
-        height: u64,
-    ) -> anyhow::Result<()> {
-        self.callback.on_header(header, height)?;
+    fn on_header(_header: &bitcoin::blockdata::block::Header, height: u64) {
         tracing::trace!(target: "parser", "on_header(height={}) called", height);
-        Ok(())
     }
 
-    /// Triggers the on_block() callback and updates statistics.
-    fn on_block(&mut self, block: &bitcoin::Block, height: u64) -> anyhow::Result<()> {
-        self.callback.on_block(block, height)?;
+    fn on_block(&mut self, _block: &bitcoin::Block, height: u64) {
         tracing::trace!(target: "parser", "on_block(height={}) called", height);
         self.print_progress(height);
-        Ok(())
     }
 
-    /// Triggers the on_complete() callback and updates statistics.
-    fn on_complete(&mut self, height: u64) -> anyhow::Result<()> {
+    fn on_complete(&mut self, height: u64) {
         tracing::info!(target: "parser", "Done. Processed blocks up to height {} in {:.2} minutes.",
         height, self.stats.started_at.elapsed().as_secs_f32() / 60.0);
 
-        self.callback.on_complete(height)?;
         tracing::trace!(target: "parser", "on_complete() called");
-        Ok(())
     }
 
     fn print_progress(&mut self, height: u64) {
