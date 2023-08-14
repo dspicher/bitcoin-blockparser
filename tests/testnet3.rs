78652c4a7e84f13f6dba85074d695ebcd1c1e18a
Dominik Spicher <dominikspicher@gmail.com>
2023-08-14 22:46:20 +0200

    tests: replicate data instead of synchronizing access
    In the future we also want to integration-test
    the higher-level `BlockchainParser` struct, which
    needs access to a proper `ChainStorage`. Our current
    approach of synchronizing access to a single one
    would lead to problems there, as it is only
    available behind a mutex guard.
    
    To solve this, we replicate the test data into a
    new tempdir for every test, which then has exclusive
    access. This also gets rid of the awkward Git
    cleanup afterwards.

--- tests/testnet3.rs
+++ tests/testnet3.rs
@@ -1,12 +1,13 @@
 mod common;
 
-static STORAGE: once_cell::sync::Lazy<
-    std::sync::Mutex<bitcoin_blockparser::parser::chain::ChainStorage>,
-> = once_cell::sync::Lazy::new(|| std::sync::Mutex::new(common::storage("testnet3", 120)));
+fn storage() -> bitcoin_blockparser::parser::chain::ChainStorage {
+    common::storage("testnet3", 120)
+}
 
 #[test]
 fn test_blockdata_parsing() {
-    let genesis = STORAGE.lock().unwrap().get_block(0).unwrap();
+    let mut storage = storage();
+    let genesis = storage.get_block(0).unwrap();
     assert_eq!(
         genesis,
         bitcoin::blockdata::constants::genesis_block(bitcoin::network::constants::Network::Testnet)
@@ -16,14 +17,15 @@ fn test_blockdata_parsing() {
         "000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"
     );
     for height in 0..=120 {
-        let block = STORAGE.lock().unwrap().get_block(height).unwrap();
+        let block = storage.get_block(height).unwrap();
         assert_eq!(block.txdata.len(), 1);
     }
 }
 
 #[test]
 fn test_genesis_header() {
-    let header = STORAGE.lock().unwrap().get_header(0).unwrap();
+    let mut storage = storage();
+    let header = storage.get_header(0).unwrap();
     assert_eq!(
         header,
         bitcoin::blockdata::constants::genesis_block(bitcoin::network::constants::Network::Testnet)
